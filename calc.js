2b0c1da9ba43cefce3d5761c7696c495f004be25
Winston <winstonjupchurch@gmail.com>
2022-01-07 20:55:05 -0600

    added functionality for numpad. working on . button, struggling cuase its like kinda more a number than a modifier but since the text content is NaN it cant pass in assignValue. also tweeked css a tad and did some minor adjustments to html.

--- calc.js
+++ calc.js
@@ -1,36 +1,82 @@
-let userInput = [1, 2 , 4, 5]; //test values will add way for it to correspond to buttons
+document.addEventListener('DOMContentLoaded', function() {
+    inputNum()
+})//primes buttons on pageload.
+
+
+const userInput = 34; //this is a test value, true value will be supplied thru buttons
 const modifiers = ['+', '-', '*', '=', '/'];
+const numpad = document.querySelector('div.numpad')
+const numbers = Array.from(numpad.querySelectorAll('div'));
+let operator = '';// final operator
+let modifier = '';// modifier being used 
+let operand = '';//final operand
+let buried = '';//held value (while user is still entering final value)
+let int;
+numbers.push(container.querySelector('.modBottom').childNodes[3])
 
-function calc(userInput){
-
+//functions
+function  displayContent(){
+let display = document.querySelector('.content');
+display.textContent =`${operator} ${modifier} ${operand}`
 }
 
-//onclick let userInput => {
-// let hold = '';
-// if(userInput.isNum) {
-// hold = userInput;
-// } else if (userInput.isNum && hold === num) {
-// hold = '' + hold + userInput;
-//} else if (userInput === NaN) {
-//  calc = hold;
-//  hold = userInput;
-// } else if (userInput === '=') {
-// final = calc + `${hold}` + hold //idea here being we have 2 held values first and last and a modifier by which the two values are to be added. after this the final needs to be stored so user can use it as value 1 for another calculation. 
-//}
-// userInput = hold; //this looks dumb butin my dumb brain hold holds the final first val while input is a modifier. then > 
-//got lost in here wont acutllay work logically but on the right track
+function assignValue(n){ // this will be called each time a number is pressed
+    if(modifier == ''){
+        console.log('Asigned to operator!')
+        operator = operator + n;
+    } else if (modifier !== ''){
+        console.log('assigned to operand!')
+        operand = operand + n;
+    }
+    displayContent()
+}
+function inputNum(){//adds event listeners still WIP
+    
+    for(let i = 0; i < numbers.length; i++) {
+        if(i == 0 || i == 4 || i == 8){
+            continue;
+        }
+        numbers[i].addEventListener('click', function(){
+            buried = numbers[i].textContent;
+            if(numbers[i].textContent == '.'){int = parseInt(buried)}
+            console.log(numbers[i].textContent)
+            assignValue(int)
+            ;
+        });
+    }
+}
+
+function clear(){//used to clear display
+let display = document.getElementById('display');
+display.textContent = '';
+}
+
+
+
+ 
+
+
+//to start no variables have been asaigned a value. when a button is pressed that number needs to be logged in a variable seperate from the input itself. the user should be able to input as many numbers as they want, this is why numbers need to be stored as strings before the final calculation becuase they need to be concatinated not added(inputting 3,3,3 is 333 not 9) this also makes it easier to intigrate period and +/- .
+//  what needs to be listened for to finalize the first number is a modifier button(+-*ect). then that modifier needs to be stored in mod and then we listen for another number. which needs to be stored seperaty from the first. then when the equals button is pressed those two numbers and the one modifier are used to do the calc. 
+//steps - 1. user enters a number. 2. user enters a modifier. 3. user enters a number. 4. user presses = and the calculation is ran. 
+
+//step one prereqs: when a number is pressed it needs to 
+    
 
 
 
 
-//calc will consist of 10 buttons fomr numbers (0-9). 6 buttons for operations (add, subtract, mulitply, devide, decimle place, and equals. there will be an interface above the numpad showing users inputs. when the user clicks a number it will be stored in an array, when the user clicks an operator button, it will inform the reduce() function that will be appied to the array. ex -> array[...ints]; let calc = (...ints) => ints.reduce(x, y) => result = x (userinput) y;
-
-// user input cannot be an array because it needs to take a single int, then a mod, then another int. so the user inputs a number and it is stored in a varible (must allow for any number of characters.) then the modifier is defined. at this point the user CAN input = if they choose but it will have no effect.
-
-//i should write a function that takes true parent node, and cycles through each of its children mapping all child nodes of all nodes to an array. reason being i made a real mess of my css/html and i dont really wanna fix it so to assign eventListeners to all of them seperately would be a bitch. 
-
-//^ !!this is all sudocode bullshit to get ideas down dont try and use this as actual declarations future me!! document.getElementById('container') => Array.from(container.childNodes) => array.forEach((array.push([this].childNodes) ... til no more childNodes. maybe figureout the real declrations for what im thinking above and wrap it all up in conditionals. onces its done we can use the resulint collection to easily apply eventListeners. 
-
-//of chance anyone other than me reads this i am stoned please do not try and undertstand. u wouldnt get it man
-
-//LOL WHY DID I WRITE THIS MUCH JFC. have fun reading this dont do drugs
+//
+// function inter(x) {
+//     if(isNaN(x) == false && holdInt === 0){
+//         holdInt = userInput;
+//         console.log('updated holdInt')
+//     } else if (isNaN(x) && holdInt === Number) {
+//         holdInt = holdInt += x;
+//         console.log('userInput added to holdInt')
+//     } else if(x === NaN && x !== '=') {
+//         mod = x;
+//         final.push(holdInt);
+//         console.log('modifier detected, updated mod / pushed holdInt into final')
+//     } else console.log('other') // unwritten atm. will run calculation of final using users chosen modifier, and push result as text content to display.
+// }
