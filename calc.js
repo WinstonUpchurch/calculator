c8643c33adfafe31e502941508dc81f85e032282
Winston <winstonjupchurch@gmail.com>
2022-01-09 19:16:46 -0600

    All mod buttons sans equals work now with the caveat that rn the +/- button does not toggle only adds but we will get there. C clears now period adds the period. almost done :)

--- calc.js
+++ calc.js
@@ -1,23 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-    inputNum()
+    inputNum(), addPeriod(), equals(), inputMod();
 })//primes buttons on pageload.
-
-
-const userInput = 34; //this is a test value, true value will be supplied thru buttons
-const modifiers = ['+', '-', '*', '=', '/'];
+//gross hard coding of single buttons ROFL
+let period = container.querySelector('.modBottom').childNodes[3] 
+function addPeriod(){period.addEventListener('click', function(){
+    return((modifier == '' ? operator = operator += '.' : operand = operand += '.'), displayContent());
+})}
+const togglenNeg = () => {
+    container.querySelector('.modBottom').childNodes[5].addEventListener('click', () => {
+        let check = 0;
+        let neg = '-';
+        if(check == 0 && modifier == ''){
+            operator = neg += operator;
+            check = 1;
+        } else if(check == 0 && modifier !== '') {
+            operand = neg += operand;
+            check = 1;
+        } else if(check == 1 && modifier == ''){
+            operator = operator.slice(1, operator.length)
+            check = 0;
+            console.log('should be working')
+        } else if(check == 1 && modifier !== ''){
+            operand = operand.slice(1, operand.length);
+            check = 0;
+        }
+        displayContent()
+        console.log(check)
+    })
+}
+const equals = () => {
+    container.querySelector('.modBR').addEventListener('click', () => console.log('test'))
+};
 const numpad = document.querySelector('div.numpad')
 const numbers = Array.from(numpad.querySelectorAll('div'));
 let operator = '';// final operator
 let modifier = '';// modifier being used 
 let operand = '';//final operand
 let buried = '';//held value (while user is still entering final value)
-let int;
-numbers.push(container.querySelector('.modBottom').childNodes[3])
+let int;//buried but not a string
 
 //functions
 function  displayContent(){
 let display = document.querySelector('.content');
-display.textContent =`${operator} ${modifier} ${operand}`
+display.textContent =`${operator} ${modifier} ${operand} `
 }
 
 function assignValue(n){ // this will be called each time a number is pressed
@@ -30,15 +55,14 @@ function assignValue(n){ // this will be called each time a number is pressed
     }
     displayContent()
 }
-function inputNum(){//adds event listeners still WIP
-    
+function inputNum(){
     for(let i = 0; i < numbers.length; i++) {
-        if(i == 0 || i == 4 || i == 8){
+        if(i == 0 || i == 4 || i == 8){//kinda gross solution to skipping containers so it doesnt double log events
             continue;
         }
         numbers[i].addEventListener('click', function(){
             buried = numbers[i].textContent;
-            if(numbers[i].textContent == '.'){int = parseInt(buried)}
+            int = parseInt(buried)
             console.log(numbers[i].textContent)
             assignValue(int)
             ;
@@ -46,37 +70,25 @@ function inputNum(){//adds event listeners still WIP
     }
 }
 
-function clear(){//used to clear display
-let display = document.getElementById('display');
-display.textContent = '';
+function calc(x, y) {
+   return(`${x}``${modifier}` `${y}`)  
 }
 
+function inputMod(){
+    let testAr = Array.from(document.querySelectorAll('.modR'));
+    console.log(testAr)
+    for (const mod of testAr) {
+        mod.addEventListener('click', () => {modifier = mod.textContent;
+        displayContent()
+        });
+}
+}
 
-
- 
-
-
-//to start no variables have been asaigned a value. when a button is pressed that number needs to be logged in a variable seperate from the input itself. the user should be able to input as many numbers as they want, this is why numbers need to be stored as strings before the final calculation becuase they need to be concatinated not added(inputting 3,3,3 is 333 not 9) this also makes it easier to intigrate period and +/- .
-//  what needs to be listened for to finalize the first number is a modifier button(+-*ect). then that modifier needs to be stored in mod and then we listen for another number. which needs to be stored seperaty from the first. then when the equals button is pressed those two numbers and the one modifier are used to do the calc. 
-//steps - 1. user enters a number. 2. user enters a modifier. 3. user enters a number. 4. user presses = and the calculation is ran. 
-
-//step one prereqs: when a number is pressed it needs to 
-    
-
-
-
-
-//
-// function inter(x) {
-//     if(isNaN(x) == false && holdInt === 0){
-//         holdInt = userInput;
-//         console.log('updated holdInt')
-//     } else if (isNaN(x) && holdInt === Number) {
-//         holdInt = holdInt += x;
-//         console.log('userInput added to holdInt')
-//     } else if(x === NaN && x !== '=') {
-//         mod = x;
-//         final.push(holdInt);
-//         console.log('modifier detected, updated mod / pushed holdInt into final')
-//     } else console.log('other') // unwritten atm. will run calculation of final using users chosen modifier, and push result as text content to display.
-// }
+function clear(){
+    let C = container.querySelector('.modBottom').childNodes[1].addEventListener('click', () => {
+       modifier = '';
+       operand = '';
+       operator = '';
+       displayContent();
+    })
+}
